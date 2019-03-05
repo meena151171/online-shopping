67837df0a78e69e502c7aa5ca812fe0508776d2a
meena151171 <mehulkumardubey@gmail.com>
2019-03-05 11:44:59 +0530

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/mehul/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mehul.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
